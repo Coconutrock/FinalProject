8ba9a4169bd0eefee3a79d1b0f7bea6ce47c2067
Coconutrock <84381235+Coconutrock@users.noreply.github.com>
2024-07-10 00:56:56 +0800

    Add files via upload

---
+++ contact-us.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/styles.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <title>Contact Us</title>
+    <link rel="icon" type="image/png" href="img/logo.png">
+    <script src="_.js "></script>
+</head>
+
+<body>
+    <?php
+    include "connection.php";
+    ?>
+    <header></header>
+    <div class="contact-us-container">
+        <div class="contact-us-section contact-us-section1">
+            <h1>Contact</h1>
+            <p>Feel Free to Contact Us </p>
+            <form action="" method="POST">
+                <input placeholder="First Name" name="fName" required><br>
+                <input placeholder="Last Name" name="lName"><br>
+                <input placeholder="E-mail Address" name="eMail" required><br>
+                <textarea placeholder="Enter your message !" name="feedback" rows="10" cols="30" required></textarea><br>
+                <button type="submit" name="submit" value="submit">Send your Message</button>
+                <?php
+                if (isset($_POST['submit'])) {
+                    $insert_query = "INSERT INTO 
+                        feedbackTable ( senderfName,
+                                        senderlName,
+                                        sendereMail,
+                                        senderfeedback)
+                        VALUES (        '" . $_POST["fName"] . "',
+                                        '" . $_POST["lName"] . "',
+                                        '" . $_POST["eMail"] . "',
+                                        '" . $_POST["feedback"] . "')";
+                    $add = mysqli_query($con, $insert_query);
+
+                    if ($add) {
+                        echo "<script>alert('Succesfully Submitted');</script>";
+                    }
+                }
+                ?>
+            </form>
+
+        </div>
+        <div class="contact-us-section contact-us-section2">
+            <h1>Address & Info</h1>
+            <h3>Phone Numbers</h3>
+            <p><a href="tel:09451187659">+63 945 118 7659</a><br>
+                <a href="tel:09588439182">+63 958 843 9182</a>
+            </p>
+            <h3>Address</h3>
+            <p> Yellow Gate, Araneta Coliseum Circle NW Quadrant, Araneta City, Quezon City</p>
+            <h3>E-mail</h3>
+            <p><a href="mailto:CinemaHub@gmail.com">CinemaHub@gmail.com</a></p>
+        </div>
+    </div>
+
+    <pre>
+    </pre>
+
+
+    <footer></footer>
+    <script src="scripts/jquery-3.3.1.min.js "></script>
+    <script src="scripts/owl.carousel.min.js "></script>
+    <script src="scripts/script.js "></script>
+</body>
+
+</html>
---
+++ database/cinema_db.sql
@@ -0,0 +1,195 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 09, 2024 at 03:15 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cinema_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookingtable`
+--
+
+CREATE TABLE `bookingtable` (
+  `bookingID` int(11) NOT NULL,
+  `movieID` int(11) DEFAULT NULL,
+  `bookingTheatre` varchar(100) NOT NULL,
+  `bookingType` varchar(100) DEFAULT NULL,
+  `bookingDate` varchar(50) NOT NULL,
+  `bookingTime` varchar(50) NOT NULL,
+  `bookingFName` varchar(100) NOT NULL,
+  `bookingLName` varchar(100) DEFAULT NULL,
+  `bookingPNumber` varchar(12) NOT NULL,
+  `bookingEmail` varchar(255) NOT NULL,
+  `amount` varchar(255) NOT NULL,
+  `ORDERID` varchar(255) NOT NULL,
+  `DATE-TIME` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `bookingtable`
+--
+
+INSERT INTO `bookingtable` (`bookingID`, `movieID`, `bookingTheatre`, `bookingType`, `bookingDate`, `bookingTime`, `bookingFName`, `bookingLName`, `bookingPNumber`, `bookingEmail`, `amount`, `ORDERID`, `DATE-TIME`) VALUES
+(71, 6, 'private-hall', 'imax', '14-3', '15-00', 'xyz', 'abc', '000000000', '000@gmail.com', '5000.00', 'cash', '2020-12-14 12:20:31'),
+(72, 1, 'main-hall', '3d', '12-3', '12-00', 'nero', 'dero', '099932242342', 'kantot@gmail.com', '300', 'ARVR87131253', '2024-07-09 18:43:31'),
+(73, 3, 'vip-hall', '3d', '1-8', '18-00', 'Phol', 'Reid', '029380482038', 'pogi@gmail.com', 'Not Paid', 'ARVR86201383', '2024-07-09 20:02:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedbacktable`
+--
+
+CREATE TABLE `feedbacktable` (
+  `msgID` int(12) NOT NULL,
+  `senderfName` varchar(50) NOT NULL,
+  `senderlName` varchar(50) DEFAULT NULL,
+  `sendereMail` varchar(100) NOT NULL,
+  `senderfeedback` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movietable`
+--
+
+CREATE TABLE `movietable` (
+  `movieID` int(11) NOT NULL,
+  `movieImg` varchar(150) NOT NULL,
+  `movieTitle` varchar(100) NOT NULL,
+  `movieGenre` varchar(50) NOT NULL,
+  `movieDuration` int(11) NOT NULL,
+  `movieRelDate` date NOT NULL,
+  `movieDirector` varchar(50) NOT NULL,
+  `movieActors` varchar(150) NOT NULL,
+  `mainhall` int(11) NOT NULL,
+  `viphall` int(11) NOT NULL,
+  `privatehall` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `movietable`
+--
+
+INSERT INTO `movietable` (`movieID`, `movieImg`, `movieTitle`, `movieGenre`, `movieDuration`, `movieRelDate`, `movieDirector`, `movieActors`, `mainhall`, `viphall`, `privatehall`) VALUES
+(1, 'img/movie 1.jpg', 'Spirited Away', 'Animation, Adventure, Drama', 127, '2021-10-18', 'Sean Anders', 'Kanna Hashimoto, Mone Kamishiraishi, Rina Kawaei, Kotaro Daigo, Hiroki Miura, Hikaru Yamano', 0, 0, 0),
+(2, 'img/movie 2.jpg', 'Garfield The Movie', 'Animation, Comedy', 101, '2024-10-18', 'Mark Dindal', 'Chris Pratt, Will Ferrell, Elizabeth Banks', 0, 0, 0),
+(3, 'img/movie 3.jpg', 'How to Make A Million Before Grandma Dies', 'Drama', 110, '2024-02-07', 'Pat Boonnitipat', 'Putthipong Assaratanakul · M ; Usha Seamkhum · Amah ; Tontawan Tantivejakul · Mui ; Sarinrat Thomas · M\'s Mother ; Sanya Kunakorn · Kiang', 0, 0, 0),
+(4, 'img/movie 4.jpg', 'The Autopsy of Jane Doe', 'Horror, Thriller, Suspense', 86, '2021-01-23', 'André Øvredal', 'Brian Cox · Tommy ; Emile Hirsch · Austin ; Ophelia Lovibond', 0, 0, 0),
+(5, 'img/movie 5.jpg', 'UNCHARTED', 'Adventure, Drama, Mystery, Action', 116, '2023-12-25', 'Ruben Fleischer', 'Tom Holland · Nathan Drake ; Mark Wahlberg · Victor Sullivan ; Antonio Banderas', 0, 0, 0),
+(6, 'img/movie 6.jpg', 'Fall Guy', 'Comedy, Action, Adventure', 126, '2024-01-04', 'David Leitch', 'Ryan Gosling · Colt Seavers ; Emily Blunt · Jody Moreno ; Aaron Taylor-Johnson', 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(80) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `password` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `name`, `password`) VALUES
+(1, '123', 'john', '123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bookingtable`
+--
+ALTER TABLE `bookingtable`
+  ADD PRIMARY KEY (`bookingID`),
+  ADD UNIQUE KEY `bookingID` (`bookingID`),
+  ADD KEY `foreign_key_movieID` (`movieID`),
+  ADD KEY `foreign_key_ORDERID` (`ORDERID`);
+
+--
+-- Indexes for table `feedbacktable`
+--
+ALTER TABLE `feedbacktable`
+  ADD PRIMARY KEY (`msgID`),
+  ADD UNIQUE KEY `msgID` (`msgID`);
+
+--
+-- Indexes for table `movietable`
+--
+ALTER TABLE `movietable`
+  ADD PRIMARY KEY (`movieID`),
+  ADD UNIQUE KEY `movieID` (`movieID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bookingtable`
+--
+ALTER TABLE `bookingtable`
+  MODIFY `bookingID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+
+--
+-- AUTO_INCREMENT for table `feedbacktable`
+--
+ALTER TABLE `feedbacktable`
+  MODIFY `msgID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `movietable`
+--
+ALTER TABLE `movietable`
+  MODIFY `movieID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookingtable`
+--
+ALTER TABLE `bookingtable`
+  ADD CONSTRAINT `foreign_key_movieID` FOREIGN KEY (`movieID`) REFERENCES `movietable` (`movieID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
