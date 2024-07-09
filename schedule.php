f3490163fdd25b0a326ff56742603c7bd8e8bee5
Coconutrock <84381235+Coconutrock@users.noreply.github.com>
2024-07-10 01:02:33 +0800

    Add files via upload

---
+++ schedule.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" href="img/logo.png">
+    <link rel="stylesheet" href="style/styles.css">
+    <title>Movies Schedule</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <script src="_.js "></script>
+</head>
+
+<header></header>
+
+<body>
+    <div class="schedule-section">
+        <h1>Schedule</h1>
+        <div class="schedule-dates">
+            <div class="schedule-item">July 15, 2024</div>
+            <div class="schedule-item schedule-item-selected">July 16, 2024</div>
+            <div class="schedule-item">July 17, 2024</div>
+            <div class="schedule-item">July 18, 2024</div>
+            <div class="schedule-item">July 19, 2024</div>
+        </div>
+        <div class="schedule-table">
+            <table>
+                <tr>
+                    <th>SHOW</th>
+                    <th>SCHEDULE IN CINEMA</th>
+                </tr>
+                <tr class="fade-scroll">
+                    <td>
+                        <h2>Spirited Away</h2>
+                        <i class="far fa-clock"></i> 127m <i class="fas fa-desktop"></i> IMAX
+                        <h3>SYNOPSIS</h3>
+                        <p>In this animated feature by noted Japanese director Hayao Miyazaki, 10-year-old Chihiro (Rumi Hiiragi) and her parents (Takashi Naitô, Yasuko Sawaguchi) stumble upon a seemingly abandoned amusement park. After her mother and father are turned into giant pigs, Chihiro meets the mysterious Haku (Miyu Irino), who explains that the park is a resort for supernatural beings who need a break from their time spent in the earthly realm, and that she must work there to free herself and her parents
+                        </p>
+                        <pre>
+                        </pre>
+                        <a class="schedule-item" href="booking.php?id=1"
+                            <div class="details-link"> 
+                            DETAILS
+                            </div>
+                        </a>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">1:00 PM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">1:00 PM</div>
+                                <div class="schedule-item">6:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">1:00 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr class="fade-scroll">
+                    <td>
+                    <h2>Garfield The Movie</h2>
+                        <i class="far fa-clock"></i> 101m <i class="fas fa-desktop"></i> IMAX
+                        <h3>SYNOPSIS</h3>
+                        <p>After an unexpected reunion with his long-lost father, the street cat Vic, Garfield is forced to leave his very comfortable life with Harald behind. Together with Vic and Odie, he plans a crazy robbery.
+                        </p>
+                        <pre>
+                        </pre>
+                        <a class="schedule-item" href="booking.php?id=2"
+                            <div class="details-link"> 
+                            DETAILS
+                            </div>
+                        </a>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr class="fade-scroll">
+                    <td>
+                    <h2>How to Make a Million Before Grandma Dies</h2>
+                        <i class="far fa-clock"></i> 110m <i class="fas fa-desktop"></i> IMAX
+                        <h3>SYNOPSIS</h3>
+                        <p>A man quits work to care for dying grandmother, motivated by her fortune. He schemes to win her favor before she passes
+                        </p>
+                        <pre>
+                        </pre>
+                        <a class="schedule-item" href="booking.php?id=3"
+                            <div class="details-link"> 
+                            DETAILS
+                            </div>
+                        </a>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr class="fade-scroll">
+                    <td>
+                    <h2>The Autopsy of Jane Doe</h2>
+                        <i class="far fa-clock"></i> 86m <i class="fas fa-desktop"></i> IMAX
+                        <h3>SYNOPSIS</h3>
+                        <p>A coroner and his son conduct an autopsy on a beautiful `Jane Doe' murder victim, and find increasingly bizarre clues.
+                        </p>
+                        <pre>
+                        </pre>
+                        <a class="schedule-item" href="booking.php?id=4"
+                            <div class="details-link"> 
+                            DETAILS
+                            </div>
+                        </a>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr class="fade-scroll">
+                    <td>
+                    <h2>UNCHARTED</h2>
+                        <i class="far fa-clock"></i> 116m <i class="fas fa-desktop"></i> IMAX
+                        <h3>SYNOPSIS</h3>
+                        <p>Treasure hunter Victor "Sully" Sullivan recruits street-smart Nathan Drake to help him recover a 500-year-old lost fortune amassed by explorer Ferdinand Magellan. What starts out as a heist soon becomes a globe-trotting, white-knuckle race to reach the prize before the ruthless Santiago Moncada can get his hands on it. If Sully and Nate can decipher the clues and solve one of the world's oldest mysteries, they stand to find $5 billion in treasure -- but only if they can learn to work together.
+                        <pre>
+                        </pre>
+                        <a class="schedule-item" href="booking.php?id=5"
+                            <div class="details-link"> 
+                            DETAILS
+                            </div>
+                        </a>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr class="fade-scroll">
+                    <td>
+                    <h2>Fall Guy</h2>
+                        <i class="far fa-clock"></i> 126m <i class="fas fa-desktop"></i> IMAX
+                        <h3>SYNOPSIS</h3>
+                        <p>After leaving the business one year earlier, battle-scarred stuntman Colt Seavers springs back into action when the star of a big studio movie suddenly disappears. As the mystery surrounding the missing actor deepens, Colt soon finds himself ensnared in a sinister plot that pushes him to the edge of a fall more dangerous than any stunt.
+                        <pre>
+                        </pre>
+                        
+
+                        
+                        
+                        <a class="schedule-item" href="booking.php?id=6"
+                            <div class="details-link"> 
+                            DETAILS
+                            </div>
+                        </a>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+
+    </div>
+    <footer></footer>
+
+    <script src="scripts/jquery-3.3.1.min.js "></script>
+    <script src="scripts/owl.carousel.min.js "></script>
+    <script src="scripts/script.js "></script>
+</body>
+
+</html>
